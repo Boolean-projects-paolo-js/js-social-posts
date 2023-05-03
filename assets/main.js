145e3e8056e01a4f9477bae3f18057fab482a18c
Paolo96700 <paolo.falcoapp@gmail.com>
2023-05-03 18:35:59 +0200

    Fine

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,17 @@
+/*
+Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Milestone 1 - Analizziamo la struttura dati fornita
+Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+BONUS
+1. Formattare le date in formato italiano (gg/mm/aaaa)
+2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+*/
+
+
+
 
 const memberPost = document.querySelector(".posts-list");
 
@@ -11,7 +25,8 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "created": "2021-06-25"
+        "created": "2021-06-25",
+        "created2": "25-06-2021"
     },
     {
         "id": 2,
@@ -22,7 +37,8 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=10"
         },
         "likes": 120,
-        "created": "2021-09-03"
+        "created": "2021-09-03",
+        "created2": "09-03-2021"
     },
     {
         "id": 3,
@@ -33,7 +49,8 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=20"
         },
         "likes": 78,
-        "created": "2021-05-15"
+        "created": "2021-05-15",
+        "created2": "15-05-2021"
     },
     {
         "id": 4,
@@ -41,10 +58,11 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=20"
         },
         "likes": 56,
-        "created": "2021-04-03"
+        "created": "2021-04-03",
+        "created2": "03-04-2021"
     },
     {
         "id": 5,
@@ -55,10 +73,16 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
-        "created": "2021-03-05"
+        "created": "2021-03-05",
+        "created2": "05-03-2021"
     }
 ];
 
+
+//Creazione posto con ciclo for
+
+
+/*
 for (i=0; i < posts.length; i++) {
     memberPost.innerHTML +=
     `<div class="post">
@@ -69,7 +93,7 @@ for (i=0; i < posts.length; i++) {
                 </div>
                 <div class="post-meta__data">
                     <div class="post-meta__author">${posts[i].author.name}</div>
-                    <div class="post-meta__time">${posts[i].created}</div>
+                    <div class="post-meta__time">${posts[i].created2}</div>
                 </div>                    
             </div>
         </div>
@@ -82,7 +106,7 @@ for (i=0; i < posts.length; i++) {
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                         <span class="like-button__label">Mi Piace</span>
                     </a>
@@ -95,3 +119,86 @@ for (i=0; i < posts.length; i++) {
     </div>
     `
 }
+*/
+
+//Creazione post con for-Each
+
+
+posts.forEach((element) => {
+    memberPost.innerHTML +=
+    `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created2}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">
+            ${element.content}
+        </div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <button class="like-button  js-like-button" data-postid="${element.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </button>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+})
+
+/*
+Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+*/
+
+const Buttonlike = document.querySelectorAll(".like-button");
+const Counters = document.querySelectorAll(".js-likes-counter");
+const arraylikedpost = []
+
+
+for (let i = 0; i < Buttonlike.length; i++){
+    const eleLike = Buttonlike[i];
+    
+    eleLike.addEventListener("click", function(){
+    
+    
+    
+      const  Counter = Counters[i];
+    
+    / il pulsate è stato già cliccato /
+    
+    if  ( eleLike.classList.contains('like-button--liked')){
+    
+        posts[i].likes -= 1
+    }
+    
+    
+    else{
+        posts[i].likes += 1
+        arraylikedpost.push(memberPost);
+        console.log(arraylikedpost);
+    }
+
+    
+    
+    eleLike.classList.toggle('like-button--liked');
+    Counter.innerHTML = `${posts[i].likes}`
+    });
+}
+
+
